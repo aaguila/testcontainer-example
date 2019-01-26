4c3b7794f3ba6dac418aa91064a602adb72ba8a7
ilun <aritz.duir@gmail.com>
2019-01-26 19:21:32 +0100

    First commit. First steps

---
+++ src/main/java/com/qajungle/todolist/Constants.java
@@ -0,0 +1,18 @@
+package com.qajungle.todolist;
+
+public class Constants {
+
+    private Constants() {}
+
+    /** API Route */
+    public static final String API_GET = "/todos/:todoId";
+    public static final String API_LIST_ALL = "/todos";
+    public static final String API_CREATE = "/todos";
+    public static final String API_UPDATE = "/todos/:todoId";
+    public static final String API_DELETE = "/todos/:todoId";
+    public static final String API_DELETE_ALL = "/todos";
+
+    /** REDIS **/
+    public static final String REDIS_TODO_KEY = "VERT_TODO";
+
+}
---
+++ src/main/java/com/qajungle/todolist/entity/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Indicates that this module contains classes that need to be generated / processed.
+ */
+@ModuleGen(name = "vertx-todo-entity", groupPackage = "com.qajungle.todolist.entity")
+package com.qajungle.todolist.entity;
+
+import io.vertx.codegen.annotations.ModuleGen;
